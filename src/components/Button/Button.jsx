4a7b69a7d435c21ca63028ce8e7cb0c0d0b37ed5
ye-butenko <ye.butenko@gmail.com>
2023-10-07 00:15:32 +0300

    hw3-p1-v2

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import { StyledButton } from './Button.styled';
-
-export const Button = ({ onLoadMore }) => {
-  return (
-    <StyledButton type="button" onClick={onLoadMore}>
-      Load more
-    </StyledButton>
-  );
-};
